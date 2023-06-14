996c61d9523fa21931eea3fc02b04744ec10ff87
SanskritiKushik5 <sanskritikushik23@gmail.com>
2023-06-14 22:01:33 +0530

    Styles

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
-import {Button, Text, View} from 'react-native';
+import {Button, Text, View, StyleSheet} from 'react-native';
 import React from 'react';
+import ExStyles from './style';
 
 const hello = () => {
   return (
     <View>
-      <Text style={{fontSize: 30, fontWeight: 'bold', color: 'blue'}}>
-        Hello React Native
-      </Text>
-      <Button title="Click me" onPress={() => alert('Hello')} />
+      <Text style={styles.textBox}>Hello React Native</Text>
+      <Button
+        style={ExStyles.button}
+        title="Click me"
+        onPress={() => alert('Hello')}
+      />
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  textBox: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'yellow',
+  },
+});
 export default hello;
---
+++ style.js
@@ -0,0 +1,8 @@
+import {StyleSheet} from 'react-native';
+
+export default ExStyles = StyleSheet.create({
+  button: {
+    marginTop: 20,
+    width: 100,
+  },
+});
